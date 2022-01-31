a0162b59951cdf46211b08f0095177991ae23e24
Luis Enriquez <lenriquez389@gmail.com>
2022-01-31 18:27:45 +0100

    Add a product (if it doesn't exist) to catalog

--- src/java/logic/CatalogManager.java
+++ src/java/logic/CatalogManager.java
@@ -11,24 +11,41 @@ import javax.faces.bean.ApplicationScoped;
 @ManagedBean
 @ApplicationScoped
 public class CatalogManager implements Serializable{
+    // ProductList permettant de stocker le catalogue
     private ArrayList<Product> productList;
+    // Variables du formulaire <h:form id="product-create">
     private int id;
     private String name;
     private double price;
 
+    // Constructeur par defaut
     public CatalogManager(){
-        this.productList = new ArrayList<Product>();
+        this.productList = new ArrayList<>();
     }
 
-    public ArrayList<Product> getProductList(){ return this.productList; }
-    public void setProductList(ArrayList<Product> l){ this.productList=l; }
+    // Setter et getter productList
+    public ArrayList<Product> getProductList(){
+        return this.productList;
+    }
+
+    public void setProductList( ArrayList<Product> productList ){
+        this.productList = productList;
+    }
+
+    // Setter et getter variables du formulaire Id, Name et Price
     public int getId() { return id; }
+
     public String getName() { return name; }
+
     public double getPrice() { return price; }
-    public void setId(int i) {this.id = i;}
-    public void setName(String N) {this.name = N;}
-    public void setPrice(double P) {this.price = P;}
     
+    public void setId( int id ) { this.id = id; }
+
+    public void setName( String N ) { this.name = N; }
+
+    public void setPrice( double P ) { this.price = P; }
+
+    // Méthode appelée après la création du bean
     @PostConstruct
     public void myInitMethod(){
         Product p1;
@@ -43,10 +60,23 @@ public class CatalogManager implements Serializable{
         this.productList.add(p3);
     }
     
+    // Utilise les variables du formulaire pour créer un nouveau produit.
+    // Puis, ajoute ce produit au catalogue si n'est pas dans le catalogue
     public String createProduct(){
         Product p;
         p = new Product(getId(), getName(), getPrice());
-        this.productList.add(p);
+
+        boolean test = false;
+
+        for ( Product element : productList ) {
+            if ( p.equals(element) ) {
+                test = true;
+                break;
+            }
+        }
+
+        if ( !test ) this.productList.add( p );
+
         return "tocatalog";
     }
 
--- src/java/model/Product.java
+++ src/java/model/Product.java
@@ -4,8 +4,11 @@ import java.io.Serializable;
 
 
 public class Product implements Serializable{
+  // identifiant du produit
   private int id;
+  // nom du produit
   private String name;
+  // prix du produit
   private double price;
   
  public Product(int id, String name, double price){
@@ -21,4 +24,18 @@ public class Product implements Serializable{
  public void setName(String N) {this.name = N;}
  public void setPrice(double P) {this.price = P;}
  
+    @Override
+    public boolean equals( Object obj ) {
+        if ( this == obj ) return true;
+        if ( obj == null ) return false;
+        if (!( obj instanceof Product ) )return false;
+
+        Product other = (Product) obj;
+
+        if ( id ==  other.getId() ) return true;
+
+        if ( name.equals( other.getName() ) ) return true;
+
+        return false;
+    }
 }
